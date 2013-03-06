c2938b1b416401b1b460bc3f6b34ce6ef60b8bd2
lecho <leszekwach@gmail.com>
2013-03-06 22:24:21 +0100

    Add TODO related to roottools.

--- rootshell/src/lecho/sample/rootshell/MainActivity.java
+++ rootshell/src/lecho/sample/rootshell/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+//TODO check roottools http://code.google.com/p/roottools/
 public class MainActivity extends Activity {
 
     @Override
